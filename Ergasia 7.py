8f66eddc585b028d289e84f0f5466a4f5c3b5bc7
KostasManiac <ligofamily@hotmail.com>
2016-10-13 20:19:53 +0300

    Αρχή της 10,modified 7.

--- Ergasia 7.py
+++ Ergasia 7.py
@@ -1,10 +1,11 @@
 #Αυτός ο αριθμός θα μας δωθεί σαν user input.
 arithmos = input("Παρακαλώ εισάγετε έναν ακέραιο αριθμό: ")
 
-def EinaiTetragonoAkeraiou(user_number):
+def einai_tetragono_akeraiou(user_number):
     """
     Boolean return για έλεγχο άμα ο αριθμός που εισήχθηκε από τον χρήστη είναι
     τετράγωνο ακεραίου.
     """
+    arithmos=int(user_number)
 
 print ("Ο αριθμός που εισάγατε είναι ο: " + arithmos)
