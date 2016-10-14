e23bcd8323a5bf9137840d4dd8d7c4cd6d9834ed
KostasManiac <ligofamily@hotmail.com>
2016-10-14 21:30:23 +0300

    Finished 7

--- Ergasia 7.py
+++ Ergasia 7.py
@@ -1,11 +1,25 @@
-#Αυτός ο αριθμός θα μας δωθεί σαν user input.
+# Αυτός ο αριθμός θα μας δωθεί σαν user input.
 arithmos = input("Παρακαλώ εισάγετε έναν ακέραιο αριθμό: ")
+arithmos = int(arithmos)
+
 
 def einai_tetragono_akeraiou(user_number):
     """
     Boolean return για έλεγχο άμα ο αριθμός που εισήχθηκε από τον χρήστη είναι
     τετράγωνο ακεραίου.
     """
-    arithmos=int(user_number)
+    if user_number == 0 or user_number == 1:
+        return False
+    else:
+        temp = user_number // 2
+        einai = [temp]
+        while temp * temp != user_number:
+            temp = (temp + (user_number // temp)) // 2
+            if temp in einai:
+                return False
+            einai.append(temp)
+        return True
 
-print ("Ο αριθμός που εισάγατε είναι ο: " + arithmos)
+
+print("Ο αριθμός που εισάγατε είναι ο: " + str(arithmos))
+print("Ο αριθμός αυτός είναι τετράγωνο ακεραίου: " + str(einai_tetragono_akeraiou(arithmos)))
